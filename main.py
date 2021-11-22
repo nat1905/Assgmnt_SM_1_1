8da6e57077c2fbfb479b2145cb6d084093d00bfa
nat1905 <ershovanatalya@gmail.com>
2021-11-22 12:52:48 +0200

    ex1.py code before changes by pylint and pycodestyle. Install pylint and pycodestyle

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from python_loc_counter import LOCCounter
+import pprint
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def loc():
+    """calculate LOC"""
+    counter = LOCCounter("ex1.py")
+    loc_data = counter.getLOC()
+    print("LOC", loc_data)
+    pprint.pprint(loc_data)
+    for k in loc_data:
+        print(f"{k}: {loc_data[k]}")
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+if __name__ == "__main__":
+    loc()
